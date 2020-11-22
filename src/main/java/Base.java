9325558db449b930815afc361671a10c1f1c3215
TIMONIN <wow.creepok.ru@mail.ru>
2020-11-22 20:27:19 +0300

    Сделал задание №3

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 
 /**
  * Прохожу курс по JAVA 2.0
@@ -13,23 +15,54 @@ public class Base {
     public static void main(String[] args) throws IOException {
 //        System.out.println("Вечер в хату, мир");
 //        System.out.printf("%d", 7845)
-
-//Задание №2 КАКУЛЯТОР НА СЛОЖЕНИЕ ДРОБНЫХ ЧИСЕЛ
-        /**
-         * Считываем вводные данные с клавиатуры, с кайфом
-         */
+        //ЗАДАНИЕ 3
+        System.out.println("Какую программу хотете запустить? \n 1 - Какалятор \n 2 - БигСлово");
         BufferedReader readr = new BufferedReader(new InputStreamReader(System.in));
-        /**
-         * Отдуши передаем данные в переменную "one" и "two" парсим в нужный формат
-         */
-        System.out.println("Введите первое число");
-        float one = Float.parseFloat(readr.readLine());
-        System.out.println("Введите второе число");
-        float two = Float.parseFloat(readr.readLine());
-        /**
-         * Складываем наши переменные и выводим результат в заданном формате
-         */
-        System.out.printf("Результат: " + "%.4f", one + two);
+        int start = Integer.parseInt(readr.readLine());
+        switch (start) {
+            case (1):
+                System.out.println("Какое действие хотите выполнить?\n 1 - Сложение.\n 2 - Вычитание. \n 3 - Умножение. \n 4 - Деление.");
+                int startCal = Integer.parseInt(readr.readLine());
+                System.out.println("Введите первое число");
+                int one = Integer.parseInt(readr.readLine());
+                System.out.println("Введите второе число");
+                int two = Integer.parseInt(readr.readLine());
+                switch (startCal) {
+                    case (1):
+                        System.out.println("Результат: " + (one + two));
+                        break;
+                    case (2):
+                        System.out.println("Результат: " + (one - two));
+                        break;
+                    case (3):
+                        System.out.println("Результат: " + (one * two));
+                        break;
+                    case (4):
+                        System.out.println("Результат: " + (one / two));
+                        break;
+                }
+                break;
+            case (2):
+                System.out.println("Заполните массив");
+                ArrayList<String> list = new ArrayList<String>();
+                while (true) {
+                    if (readr.readLine().equals("")) {
+                        break;
+                    }
+                    else
+                        list.add(readr.readLine());
+                }
+                int i = 0;
+                int cur, indexOfMax = 0, maxLen = list.get(0).length();
+                while (i < list.size() - 1) {
+                    cur = list.get(++i).length();
+                    if (cur > maxLen) {
+                        indexOfMax = i;
+                        maxLen = cur;
+                    }
+                }
+                System.out.println("Слово максимальной длины: " + list.get(indexOfMax) + ", длина: " + maxLen);
 
+        }
     }
 }
