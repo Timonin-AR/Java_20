59185322fd30be15fe21c5aa3881d082aa51360d
TIMONIN <wow.creepok.ru@mail.ru>
2020-12-20 21:52:02 +0300

    Выполненное задание №6:
    Описание:
    Есть входной файл с набором слов, написанных через пробел.
    Необходимо:
    Прочитать слова из файла.
    Отсортировать в алфавитном порядке.
    Посчитать сколько раз каждое слово встречается в файле.
    Вывести статистику на консоль
    Найти слово с максимальным количеством повторений.
    Вывести на консоль это слово и сколько раз оно стречается в файле

---
+++ src/main/java/StatisticsWords/ScanFile.java
@@ -0,0 +1,62 @@
+package StatisticsWords;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ScanFile {
+    public static String scanFile(String path) throws IOException {
+        StringBuilder str = new StringBuilder();
+
+        FileReader fr = new FileReader(path);
+        Scanner scan = new Scanner(fr);
+
+        while (scan.hasNextLine()) {
+            str.append(scan.nextLine());
+        }
+
+        fr.close();
+        return str.toString();
+    }
+
+    public static String[] splitStringToList(String str) {
+        String[] strings;
+        strings = str.split(" ");
+
+        return strings;
+    }
+
+    public static Map<String, Integer> countingWords(String[] array) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            String a = array[i];
+            int scour = 0;
+
+            for (int j = 0; j < array.length; j++) {
+
+                if (a.equals(array[j])) {
+                    scour++;
+                }
+            }
+            map.put(a, scour);
+            scour = 0;
+        }
+        return map;
+    }
+
+    public static Map<String, Integer> wordMaxRepeat(Map<String, Integer> map) {
+        Map<String, Integer> map1 = new HashMap<String, Integer>();
+        int maxValueInMap = (Collections.max(map.values()));
+        for (Map.Entry entry : map.entrySet()) {
+            if (entry.getValue().equals(maxValueInMap)) {
+                map1.put((String) entry.getKey(), (Integer) entry.getValue());
+
+            }
+        }
+        return map1;
+    }
+}
---
+++ src/main/java/StatisticsWords/StatisticsWords.java
@@ -0,0 +1,39 @@
+package StatisticsWords;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+public class StatisticsWords {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Считываем файл...");
+        String b = ScanFile.scanFile("C:\\Users\\TIMONIN\\IdeaProjects\\Java 2.0\\src\\main\\java\\StatisticsWords\\text.txt");
+        System.out.println("Файл считан. В нем лежат слова: \n" + b);
+        String[] a = ScanFile.splitStringToList(b);
+        System.out.println("Разделяю слова, чтобы их подсчитать:");
+        for (String strings : a) {
+            System.out.println(strings);
+        }
+        Arrays.sort(a);
+        System.out.println("Сортирую данный список слов по алфавиту\n");
+        for (String strings : a) {
+            System.out.println(strings);
+        }
+        System.out.println("Вывожу статистику повторов:\n");
+        Map<String, Integer> maps = ScanFile.countingWords(a);
+        for (Map.Entry entry : maps.entrySet()) {
+            System.out.println("Слово: " + entry.getKey() + " "
+                    + entry.getValue() + " раз повторилось. ");
+        }
+        System.out.println("Ищу самое повторяемое слово:\n\n");
+        maps = ScanFile.wordMaxRepeat(maps);
+        for (Map.Entry entry : maps.entrySet()) {
+            System.out.println("Больше всего повторилось слово: " + entry.getKey() + " "
+                    + entry.getValue() + " раз! ");
+        }
+
+    }
+
+
+}
+
